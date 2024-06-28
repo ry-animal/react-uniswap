1e6473d41bd0ca402484e8e3b5f470d1e33aea71
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-06-27 19:27:39 -0700

    add rpcs

--- src/constants.ts
+++ src/constants.ts
@@ -45,7 +45,14 @@ export const tokenList: Token[] = [
 const uniswapV2Factory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 const uniswapV2Router = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
+const quickNodeSepolia =
+  'https://virulent-wiser-arrow.ethereum-sepolia.quiknode.pro/8fa662b94129000a65aaa56c5409ab9c11a3af75/';
+
+const quickNodeMainnet = 'https://eth.llamarpc.com/';
+
 export const addresses = {
   uniswapV2Factory,
   uniswapV2Router,
+  quickNodeSepolia,
+  quickNodeMainnet,
 };
