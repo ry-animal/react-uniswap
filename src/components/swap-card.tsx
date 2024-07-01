28b672369ddaddb6833195183d88f5953ca68f1a
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-01 00:21:18 -0700

    fix accidental delete and comments

--- src/components/swap-card.tsx
+++ src/components/swap-card.tsx
@@ -445,7 +445,7 @@ const SwapCard: React.FC = () => {
 
   const getEthPrice = useCallback(async () => {
     try {
-      const response = await fetch('https
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
       const data = await response.json();
       setEthPrice(data.ethereum.usd);
     } catch (error) {
