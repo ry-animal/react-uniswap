a1faa9836901570efc3d844cb73ded7226f68070
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-01 00:03:00 -0700

    remove setupTests.ts

--- src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-expect.extend({});
-
-afterEach(() => {
-  cleanup();
-});
